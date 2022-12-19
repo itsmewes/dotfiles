1c809e3738d807ed57caf0802a062d8d3da6f27d
Wesley Martin <wesley.martin@carandclassic.com>
2022-12-19 08:27:00 +0200

    Trying out new nvim configs

--- nvim/lua/wm/plugins/github-nvim-theme.lua
+++
@@ -1,6 +0,0 @@
-require("github-theme").setup({
-  theme_style = "dark",
-  dark_sidebar = false,
-
-  colors = {bg = "#1d1f21"},
-})
--- nvim/lua/wm/plugins/nord.lua
+++
@@ -1,3 +0,0 @@
-vim.g.nord_cursorline_transparent = true
-
-vim.cmd("colorscheme nord")
